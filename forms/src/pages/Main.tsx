3d8d63d7e2dbd606d159698563fb2821300dd8b3
arastepa <stepanyan2298@gmail.com>
2024-08-18 01:43:06 +0400

    fix: uncontrolled

--- forms/src/pages/Main.tsx
+++ forms/src/pages/Main.tsx
@@ -1,5 +1,27 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store.ts';
+import styles from './Main.module.css';
+
 const Main = () => {
-  return <div>Content</div>;
+  const storeData = useSelector((state: RootState) => state.formData);
+  return (
+    <div className={styles.main}>
+      {storeData.data.map((item) => {
+        return (
+          <div>
+            <p>name {item.name}</p>
+            <p>age {item.age}</p>
+            <p>country {item.country}</p>
+            <p>email {item.email}</p>
+            <p>gender {item.gender}</p>
+            <p>
+              <img src={item.picture} alt="" />
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Main;
